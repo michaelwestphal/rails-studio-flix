16551ab4cd6f9d6fb606cf16f533044ebbb744b0
Michael Westphal <michaelwestphal@hey.com>
2023-01-15 14:40:20 -0600

    Chapter 10 and 11: Routes - Show Page and Linking

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "movies#index"
+
   get "movies" => "movies#index"
+  get "movies/:id" => "movies#show", as: "movie"
 end
