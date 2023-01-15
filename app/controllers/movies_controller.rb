8a387613114ca561119b117b3f5543a92a057f52
Michael Westphal <michaelwestphal@hey.com>
2023-01-15 16:11:17 -0600

    Chapter 12: Forms - Editing Records

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,23 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    # Use "fail" trick to view what is coming through via the Rails error page
+    # fail
+    @movie = Movie.find(params[:id])
+    movie_params =
+      params.require(:movie).
+        permit(:title, :description, :rating, :released_on, :total_gross)
+    # Alternative:
+    # params.require(:movie).permit!
+    #  While convenient, using `permit!` is risky because all the attributes will always be updatable from form data.
+    #  Instead, it's better to explicitly list the attributes that can be updated from a form.
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
 end
