b29a44a3af770a09ad89bf3daa2eaa1fe04f761b
Michael Westphal <michaelwestphal@hey.com>
2023-05-21 19:01:19 -0500

    Chapter 24: One-to-Many - Forms

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,34 @@
 class ReviewsController < ApplicationController
+  # Can append "only: [:action_method_name]" or "except: [:action_method_name]"
+  before_action :set_movie
+
   def index
-    @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
+  end
+
+  def new
+    @review = @movie.reviews.new
+  end
+
+  def create
+    @review = @movie.reviews.new(review_params)
+    # A REALLY long form version that appear to work. (In need to get better with Ruby hashes and symbols!!)
+    # @review = @movie.reviews.new({ name: params[:review][:name], stars: params[:review][:stars], comment: params[:review][:comment] })
+
+    if @review.save
+      redirect_to movie_review_path(@movie), notice: "Thanks for your review!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:name, :stars, :comment)
   end
 end
