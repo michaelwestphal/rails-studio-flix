ee24fdcfc17c17f93e1ada9cc740bde7b0ca084f
Michael Westphal <michaelwestphal@hey.com>
2023-06-11 14:12:15 -0500

    Bonus: Add min 10 password length validation
    And allow it to be blank so the validation can be skipped if they are
    updating the user details while wanting to leave the password alone.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   validates :email, presence: true,
                     format: { with: /\S+@\S+/ },
                     uniqueness: { case_sensitive: false }
+
+  validates :password, length: { minimum: 10, allow_blank: true }
 end
