d63106d7030201dd4962606d3b591e364e35d985
Michael Westphal <michaelwestphal@hey.com>
2023-06-25 15:56:26 -0500

    Chapter 27: User Signup
    With extra credit section too.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
 module UsersHelper
+  def profile_image(user)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{user.gravatar_id}"
+    image_tag(gravatar_url, alt: "#{user.name} avatar")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "movies#index"
+  root 'movies#index'
 
   resources :movies do
     resources :reviews
   end
 
   resources :users
+
+  # Which way?
+  # get 'signup', to: 'users#new'
+  get 'signup' => 'users#new'
 end
