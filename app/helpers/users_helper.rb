ff314efa6f8d9404ca196b8982ccc4a3d2f7e915
Michael Westphal <michaelwestphal@hey.com>
2023-08-09 16:56:00 -0500

    Bonus: Add reviewer profile image
    And username

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
-  def profile_image(user)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{user.gravatar_id}"
+  def profile_image(user, size=80)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{user.gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: "#{user.name} avatar")
   end
 end
