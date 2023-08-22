bf1c15e8d4333e0218546384452dd362cde1ff66
Michael Westphal <michaelwestphal@hey.com>
2023-08-22 07:23:11 -0500

    Chapter 39: Custom Scopes and Routes - Part 1
    Convert the class level released method into a scope. I commented out
    the original since it had lots of notes and as a reference. Normally I
    would have deleted it.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -25,27 +25,32 @@ class Movie < ApplicationRecord
   #  OR
   # validates :rating, inclusion: { in: RATINGS }
 
-  # NOTE: Class level method
-  def self.released
-    # NOTE: The placeholder, '?', is needed to convert the Ruby type into a valid
-    #  SQL type. Otherwise we would need to convert it ourselves if we wanted to
-    #  use String interpolation. (There's also a question of properly sanitized inputs
-    #  if we use interpolation. I'm guessing we're responsible for this if we chose that
-    #  means.)
-    where('released_on < ?', Time.now).order(released_on: :desc)
-    # OR
-    # where("released_on < ?", Time.now).order("released_on desc")
+  scope :released, -> { where('released_on < ?', Time.now).order(released_on: :desc) }
+  scope :recent, ->(max = 5) { released.limit(max) }
+  # OR explicitly denote this as a lambda:
+  # scope :recent, lambda { |max = 5|  released.limit(max) }
 
-    # EXAMPLES from the course of some predicates:
-    #
-    # Movie.where("total_gross < 225000000")
-    # Movie.where("total_gross < ?", 225_000_000)
-    # Movie.where(rating: "PG-13")
-    # Movie.where("rating = 'PG-13'")
-    # Movie.where(total_gross: 250_000_000..900_000_000)
-    # Movie.where(total_gross: 250_000_000..)
-    # Movie.where(total_gross: ..900_000_000)
-  end
+  # NOTE: Class level method
+  # def self.released
+  #   # NOTE: The placeholder, '?', is needed to convert the Ruby type into a valid
+  #   #  SQL type. Otherwise we would need to convert it ourselves if we wanted to
+  #   #  use String interpolation. (There's also a question of properly sanitized inputs
+  #   #  if we use interpolation. I'm guessing we're responsible for this if we chose that
+  #   #  means.)
+  #   where('released_on < ?', Time.now).order(released_on: :desc)
+  #   # OR
+  #   # where("released_on < ?", Time.now).order("released_on desc")
+  #
+  #   # EXAMPLES from the course of some predicates:
+  #   #
+  #   # Movie.where("total_gross < 225000000")
+  #   # Movie.where("total_gross < ?", 225_000_000)
+  #   # Movie.where(rating: "PG-13")
+  #   # Movie.where("rating = 'PG-13'")
+  #   # Movie.where(total_gross: 250_000_000..900_000_000)
+  #   # Movie.where(total_gross: 250_000_000..)
+  #   # Movie.where(total_gross: ..900_000_000)
+  # end
 
   def flop?
     # My horribly non-Rubyesc solution:
